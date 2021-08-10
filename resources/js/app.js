172faa66e23dac774c50c6caf8b3c38eeb3ff40f
Jingoo Kim <kimjingo@gmail.com>
2021-08-09 22:37:31 -0700

    built api and vue front-end

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import App from './vue/app'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// library.add(faUserSecret)
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});
